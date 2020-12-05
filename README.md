5df949447d665074295a5f528457056ec061ba60
Fred Seeley <fred.seeley@gmail.com>
2020-12-05 17:06:37 +0000

    Day 5 complete

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 | [Day 2: Password Philosophy](day%202) | ⭐ | ⭐ |
 | [Day 3: Toboggan Trajectory](day%203) | ⭐ | ⭐ |
 | [Day 4: Passport Processing](day%204) | ⭐ | ⭐ |
+| [Day 5: Binary Boarding](day%205) | ⭐ | ⭐ |
---
+++ day 5/aoc5.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+    with open(sys.argv[1]) as f:
+        ids = {int(''.join(['1' if x in 'BR' else '0' for x in bp]), 2) for bp in f.read().split()}
+
+        p1 = max(ids)
+        p2 = max({*range(max(ids))} - ids)
+
+
+    print(p1)
+    print(p2)
+
+
+if __name__ == "__main__":
+    main()
